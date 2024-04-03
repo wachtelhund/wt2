88b13386a96f811eda40743b43ef3d308aba4756
wachtelhund <hampus.nilsson222@hotmail.se>
2024-04-03 13:24:52 +0200

    [production] Add docker compose

--- api/api/main.py
+++ api/api/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from api.routes import main_router
 
-
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 main_router.MainRouter(app).attach_routes()
 
