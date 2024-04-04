ed3f2f0a270be2b715dac61b379b0c8f457b9797
wachtelhund <hampus.nilsson222@hotmail.se>
2024-04-04 13:17:02 +0200

    [server] Add filtration

--- api/api/controllers/store_controller.py
+++ api/api/controllers/store_controller.py
@@ -5,5 +5,18 @@ class StoreController:
     def __init__(self, data_reader: DataReader):
         self.data_reader = data_reader
     
-    def get_entries(self, page: int, page_size: int):
-        return self.data_reader.get_entries(page, page_size)
+    def get_entries(self, query: PaginatedRequest):
+        if query.filter_by and query.filter_value:
+            filtered_data = self.data_reader.get_matching_entries(query.filter_by, query.filter_value, query.page, query.page_size)
+        else:
+            filtered_data = self.data_reader.get_entries(query.page, query.page_size)
+        if query.sort_by:
+            filtered_data = sorted(filtered_data, key=lambda x: x[query.sort_by], reverse=query.sort_desc)
+        return filtered_data
+    
+    def get_ids(self):
+        return self.data_reader.get_unique_values("Store")
+    
+    def get_matching_entries(self, store_id: str):
+        return self.data_reader.get_matching_entries("Store", store_id)
+    
--- api/api/model/data_reader.py
+++ api/api/model/data_reader.py
@@ -7,10 +7,12 @@ class DataReader():
         self.data = None
         self.path_csv_file = path_csv_file
         self.load_data()
+        self.count = len(self.data)
         self.clean_data()
 
     def load_data(self):
         self.data = read_csv(self.path_csv_file)
+        self.data.columns = self.data.columns.str.strip().str.lower().str.replace(" ", "_")
 
     def clean_data(self):
         self.data = self.data.replace([np.nan, np.inf, -np.inf], None)
@@ -21,5 +23,23 @@ class DataReader():
         return self.data.iloc[start:end].to_dict(orient="records")
     
     def get_all_entries(self):
-        return self.data.to_dict(orient="records")
+        data = self.data.to_dict(orient="records")
+        data.append({"count": self.count})
+        return data
+    
+    def get_unique_values(self, column: str):
+        return self.data[column].unique().tolist()
+    
+    def get_matching_entries(self, column: str, value: str, page: int, page_size: int):
+        if column not in self.data.columns:
+            print(f"Column '{column}' not found in DataFrame.")
+            return []
+        filtered_df = self.data[self.data[column].astype(str).str.lower() == value.lower()]
+        filteredValue = filtered_df.to_dict(orient="records")
+        start = (page - 1) * page_size
+        end = start + page_size
+        return filteredValue[start:end]
+
+    def get_count(self):
+        return self.count
 
