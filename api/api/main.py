a9ac04b62b01d104c41590abf5f8f02bf3c303e6
wachtelhund <hampus.nilsson222@hotmail.se>
2024-04-18 10:39:03 +0200

    [api/client] Add cors, add base href to client

--- api/api/main.py
+++ api/api/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:4200"],
+    allow_origins=["http://localhost:4200", "https://cscloud8-49.lnu.se"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
